6bb76ccb0017cbfa3ab535cd89f18723f23f686a
Zhou Yuntao <zhouyuntao@magictavern.com>
2017-02-24 21:27:33 +0800

    rebuild local packages when build final app

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 rm -rf ~/Downloads/rocket.chat.build/bundle
-meteor build --directory ~/Downloads/rocket.chat.build/ --server-only --architecture os.linux.x86_64
+rm -rf .meteor/local/isopacks
+meteor build --headless --directory ~/Downloads/rocket.chat.build/ --server-only --architecture os.linux.x86_64
 
