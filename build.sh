c00a5bf44d9c9486d1b94249d15bc819d7a7c4b3
Yuntao Zhou <zhouyuntao@magictavern.com>
2017-03-04 23:28:44 +0800

    build.sh: pass extra arguments

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 
 rm -rf ~/Downloads/rocket.chat.build/bundle
 rm -rf .meteor/local/isopacks
-meteor build --headless --directory ~/Downloads/rocket.chat.build/ --server-only --architecture os.linux.x86_64
+meteor build --headless --directory ~/Downloads/rocket.chat.build/ --server-only --architecture os.linux.x86_64 "$@"
 
